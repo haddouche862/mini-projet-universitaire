5643a34135a94b20f7b2f23f0bfdab47e8bbd0e7
Ines-e <ines.yahiatene@etu.unilim.fr>
2024-12-15 14:23:37 +0100

    Update cours-enseignants.php

--- cours-enseignants.php
+++ cours-enseignants.php
@@ -29,6 +29,7 @@ $cours = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style.css">
     <title>Mes cours</title>
 </head>
 <body>
